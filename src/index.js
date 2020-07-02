c442cdf5fcee337aec17aed86c6ca00fc5093f31
jbaah69 <46765933+jbaah69@users.noreply.github.com>
2020-07-02 00:34:58 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "Green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "Blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
